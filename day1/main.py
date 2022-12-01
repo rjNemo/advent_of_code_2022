b78eb75e4d8d0c53632f9f4fed04d77c9d1e3fb5
Ruidy <ruidy.nemausat@gmail.com>
2022-12-01 21:30:47 +0100

    day1 part 2

--- day1/main.py
+++ day1/main.py
@@ -1,4 +1,4 @@
-def max_calories() -> int:
+def max_calories_1() -> int:
     with open("./input.txt", "r") as f:
         calories = 0
         tmp = 0
@@ -11,5 +11,20 @@ def max_calories() -> int:
     return calories
 
 
+def max_calories_2() -> int:
+    with open("./input.txt", "r") as f:
+        calories = []
+        tmp = 0
+        for line in f.readlines():
+            if (c := line.strip()) != "":
+                tmp += int(c)
+            else:
+                calories.append(tmp)
+                calories.sort(reverse=True)
+                calories = calories[:3]
+                tmp = 0
+    return sum(calories)
+
+
 if __name__ == "__main__":
-    print(max_calories())
+    print(max_calories_2())
