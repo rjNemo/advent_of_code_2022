62a1897a3575b267217449735092e87f2552ef26
Ruidy <ruidy.nemausat@gmail.com>
2022-12-03 19:56:24 +0100

    day3 part 2

--- day1/README.md
+++ day1/README.md
@@ -52,7 +52,7 @@ fourth Elf).
 
 Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
 
-## --- Part Two ---
+## Part Two
 
 By the time you calculate the answer to the Elves' question, they've already realized that the Elf carrying the most
 Calories of food might eventually run out of snacks.
--- day3/main.py
+++ day3/main.py
@@ -17,7 +17,7 @@ def priority_shared_items_sum_1(data: list[str]) -> int:
 def process(rucksack: str) -> int:
     size = len(rucksack)
     a = rucksack[: size // 2]
-    b = rucksack[size // 2 :]
+    b = rucksack[size // 2:]
     a = set(a)
     b = set(b)
     item = a.intersection(b)
@@ -29,7 +29,26 @@ def read_data() -> list[str]:
         return [line.strip() for line in f.readlines()]
 
 
+def priority_shared_items_sum_2(data: list[str]) -> int:
+    badges = []
+    i = 0
+    while i < len(data):
+        group = [set(line) for line in data[i: i + 3]]
+        badge = find_intersection(group)
+        badges.append(ascii_letters.index(badge) + 1)
+        i += 3
+    return sum(badges)
+
+
+def find_intersection(group: list[set[str]]) -> str:
+    return group[0].intersection(group[1]).intersection(group[2]).pop()
+
+
 if __name__ == "__main__":
     dataset = read_data()
+
     res = priority_shared_items_sum_1(dataset)
     assert res == 157, f"{res} is not the right value"
+
+    res = priority_shared_items_sum_2(dataset)
+    assert res == 70, f"{res} is not the right value"
