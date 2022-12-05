9d19a950738eb99dfe1b95686f940d617d983e08
Ruidy <ruidy.nemausat@gmail.com>
2022-12-05 14:45:41 +0100

    day5 part 2

--- day5/main.py
+++ day5/main.py
@@ -22,6 +22,38 @@ class Move:
     to: int
 
 
+def top_crates_1(data: list) -> str:
+    stacks, instructions = process_data(data)
+    stacks = find_top_crates_1(stacks, instructions)
+    return output_top_crates(stacks)
+
+
+def find_top_crates_1(stacks: list[list[str]], instructions: list[Move]) -> list[list[str]]:
+    for step in instructions:
+        for _ in range(step.quantity):
+            stacks[step.to].append(stacks[step.of].pop())
+
+    return stacks
+
+
+def find_top_crates_2(stacks: list[list[str]], instructions: list[Move]) -> list[list[str]]:
+    for step in instructions:
+        tmp = []
+        print(tmp)
+        for _ in range(step.quantity):
+            tmp.insert(0, stacks[step.of].pop())
+            print(tmp)
+        stacks[step.to].extend(tmp)
+
+    return stacks
+
+
+def top_crates_2(data: list[str]) -> str:
+    stacks, instructions = process_data(data)
+    stacks = find_top_crates_2(stacks, instructions)
+    return output_top_crates(stacks)
+
+
 def process_data(data: list[str]) -> tuple[list[list[str]], list[Move]]:
     rows = []
     steps = []
@@ -66,21 +98,15 @@ def _build_instructions(instructions: list[str]) -> list[Move]:
     return moves
 
 
-def find_top_crates(stacks: list[list[str]], instructions: list[Move]) -> str:
-    for step in instructions:
-        for _ in range(step.quantity):
-            stacks[step.to].append(stacks[step.of].pop())
-
+def output_top_crates(stacks) -> str:
     return "".join(stack[-1] for stack in stacks)
-
-
-def top_crates(data: list) -> str:
-    stacks, instructions = process_data(data)
-    return find_top_crates(stacks, instructions)
 
 
 if __name__ == "__main__":
     dataset = read_data()
 
-    res = top_crates(dataset)
+    res = top_crates_1(dataset)
     assert res == "CMZ", f"{res} is not the right value"
+
+    res = top_crates_2(dataset)
+    assert res == "MCD", f"{res} is not the right value"
